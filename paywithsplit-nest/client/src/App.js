0f01570666da94a5c8981bae1d2a180236096cc7
Anton Bergman <antonbergman_@hotmail.com>
2019-03-27 21:39:54 +0100

    Add first react components

--- paywithsplit-nest/client/src/App.js
+++ paywithsplit-nest/client/src/App.js
@@ -3,25 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked : false
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
+  } 
+
+  _onButtonClick() {
+    this.setState({
+      clicked: true
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {!this.state.clicked ? <BuyButton onClick={this._onButtonClick}/> : null}
+        {this.state.clicked ? <PurchaseForm/> : null}
       </div>
-    );
+    )
+  }
+}
+
+class BuyButton extends Component {
+  render() {
+    return (
+      <button {...this.props}>
+        {
+          "Buy with Split"
+        }
+      </button>
+    )
+  }
+}
+
+class PurchaseForm extends Component {
+  render() {
+    return (
+      <div {...this.props}> Purchase Form Component</div>
+    )
   }
 }
 
