5ac335f3b4192d0afd217ecae3f0fdb674bded04
Anton Bergman <antonbergman_@hotmail.com>
2019-04-08 21:49:07 +0200

    add puppeteer deps and first parsing of website

--- paywithsplit-nest/src/subscribers/subscribers.controller.ts
+++ paywithsplit-nest/src/subscribers/subscribers.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Param, Post, Body, Delete, Query, Logger} from '@nestj
 import { SubscribersService } from './subscribers.service';
 import { CreateSubscriberDTO } from './dto/create-subscriber.dto';
 
+const puppeteer = require('puppeteer')
+
 @Controller('subscribers')
 export class SubscribersController {
     constructor(private subscriberService: SubscribersService) {}
@@ -25,7 +27,38 @@ export class SubscribersController {
         let stringdata = JSON.stringify(data, null, 2);
         this.logger.log('subscriber controller recieved post! body: ', stringdata);
         const subscribers = await this.subscriberService.addSubscriber(data);
-        return subscribers;
+
+        async function run() {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto('http://localhost:3000/NormalCheckout');
+
+            const buttonSelector = '#root > div > div:nth-child(1) > div > div:nth-child(2) > button';
+            await page.waitForSelector(buttonSelector);
+            await page.click(buttonSelector);
+            
+
+            await page.waitFor(3000);
+            await page.screenshot({path: './screenshots/example1.png'});
+            
+            // TODO: Fetch these values from data
+
+            const frame = await page.frames().find(f => f.name() === 'stripe_checkout_app');
+            await frame.$eval('[placeholder="Email"]', el => el.value = "antonbargman@gmail.com");
+            await frame.$eval('[placeholder="Name"]', el => el.value = "Anton Bergman");
+            await frame.$eval('[placeholder="Street"]', el => el.value = "Lemansgatan 10");
+            await frame.$eval('[placeholder="Postcode"]', el => el.value = "41260");
+            await frame.$eval('[placeholder="City"]', el => el.value = "Gothenburg");
+            
+            await page.screenshot({path: './screenshots/example2.png'});
+            browser.close();
+        }
+
+        
+
+        run().catch(err => console.error(err));
+
+        return subscribers
     }
 
     @Delete()
@@ -33,6 +66,4 @@ export class SubscribersController {
         const subscribers = await this.subscriberService.deleteSubscriber(query.subscriberId);
         return subscribers;
     }
-
-
 }
